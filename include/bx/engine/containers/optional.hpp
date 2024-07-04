dc2a52dbe8259c6027d6aa1454853045fbdfee88
Jason de Wolff <jasondewolff@gmail.com>
2024-07-04 19:57:56 +0200

    made optional smaller

--- include/bx/engine/containers/optional.hpp
+++ include/bx/engine/containers/optional.hpp
@@ -23,38 +23,37 @@ public:
 
 	b8 IsSome() const
 	{
-		return isSome;
+		return data != nullptr;
 	}
 
 	b8 IsNone() const
 	{
-		return !isSome;
+		return data == nullptr;
 	}
 
 	T& Unwrap()
 	{
-		BX_ASSERT(isSome, "Unwrap on a None value.");
+		BX_ASSERT(data, "Unwrap on a None value.");
 		return *data;
 	}
 
 	const T& Unwrap() const
 	{
-		BX_ASSERT(isSome, "Unwrap on a None value.");
+		BX_ASSERT(data, "Unwrap on a None value.");
 		return *data;
 	}
 
 private:
 	Optional()
-		: data(nullptr), isSome(false)
+		: data(nullptr)
 	{}
 
 	template <typename ...Params>
 	Optional(Params&&... params)
-		: data(new T(std::forward<Params>(params)...)), isSome(true)
+		: data(new T(std::forward<Params>(params)...))
 	{}
 
 	// TODO: Optional<T> does not support copying, even if T itself does
 	// Replacing unique_ptr with a raw ptr and calling copy constructors when relevant can add the ability to copy Optional<T> when T implements copy constructors
 	std::unique_ptr<T> data;
-	b8 isSome;
 };
